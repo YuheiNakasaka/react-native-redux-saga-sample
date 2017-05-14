6dd7e234c0152234c9a451bb0473fc336296a81e
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2017-05-15 00:48:14 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [
+          1, {
+            "extensions": [".js", ".jsx"]
+          }
+        ],
+        "react/prefer-stateless-function": [
+          0, { "ignorePureComponents": true}
+        ]
+    }
+};
